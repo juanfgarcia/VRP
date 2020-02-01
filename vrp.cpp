93c4c5027a0271e0d38214a7f2074d6041f1b989
Juan Francisco Garcia Casado <juanfgarcia@gmail.com>
2020-02-01 17:30:25 +0100

    Set of state ordered by it's f

--- vrp.cpp
+++ vrp.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
 #include <string>
+#include <set>
 #ifndef STATE_H
 #include "state.hpp"
 #endif
 using namespace std;
+
+
+struct state_compare {
+    bool operator() (const State *lhs, const State *rhs) const {
+        return lhs->f < rhs->f;
+    }
+};
+
+typedef set<State*, state_compare> state_set; 
+
 
 int main(int argc, char **argv) {
     if (argc!=2){
@@ -13,10 +24,15 @@ int main(int argc, char **argv) {
     string input_file = argv[1];
    
     State* initial_state = new State(input_file);   
+    state_set open;
+    open.insert(initial_state);
 
     for (State* state : initial_state->getSuccessors()){
-        cout << state->f << endl;
+        open.insert(state);
     }
     
+    for (State* state: open){
+        cout << state->f << endl;
+    }
     return 0;
 }
